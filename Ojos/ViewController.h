5dee5fb669a4a999c86cbda8ce65411a985868f5
Trevor Grayson <tgrayson@oddz.com>
2015-09-16 16:57:36 -0700

    Initial Commit

---
+++ Ojos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ojos
+//
+//  Created by Trevor Grayson on 9/16/15.
+//  Copyright (c) 2015 Trevor Grayson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
